a3aad99dd906d1c78a9e93c294ef4bbbb6f1f8c6
Andras Schaffer <eosbandi@gmail.com>
2020-02-17 00:11:25 +0100

    Code cleanup Added comments Rewrite Invoke parts

--- RACPluginTensionerStat.cs
+++ RACPluginTensionerStat.cs
@@ -27,12 +27,12 @@ namespace RACPluginTensionerStat
     public class Plugin : MissionPlanner.Plugin.Plugin
     {
 
-        SplitContainer sc;
-        TableLayoutPanel tblMap;
-        Label lab;
+        SplitContainer FDRightSide;
+        Label lDebugInfo;
         float tension_value;
         string urlTensionerAddress;
         int webTimeoutMs;
+
 
         public override string Name
         {
@@ -41,65 +41,48 @@ namespace RACPluginTensionerStat
 
         public override string Version
         {
-            get { return "0.1"; }
+            get { return "0.2"; }
         }
 
         public override string Author
         {
-            get { return "Andras Schaffer"; }
+            get { return "Andras Schaffer / RotorsAndCams"; }
         }
 
-        //[DebuggerHidden]
         public override bool Init()
         {
 
             loopratehz = 2;
 
-            MainV2.instance.Invoke((Action)
-     delegate
-     {
 
-
-         sc = Host.MainForm.FlightData.Controls.Find("splitContainer1", true).FirstOrDefault() as SplitContainer;
-         //TrackBar tb = Host.MainForm.FlightData.Controls.Find("TRK_zoom", true).FirstOrDefault() as TrackBar;
-         //Panel pn1 = Host.MainForm.FlightData.Controls.Find("panel1", true).FirstOrDefault() as Panel;
-         //tblMap = Host.MainForm.FlightData.Controls.Find("tableMap", true).FirstOrDefault() as TableLayoutPanel;
-         //SplitContainer SubMainLeft = Host.MainForm.FlightData.Controls.Find("SubMainLeft", true).FirstOrDefault() as SplitContainer;
-         //HUD hud = SubMainLeft.Panel1.Controls["hud1"] as HUD;
-
-         lab = new System.Windows.Forms.Label();
-         lab.Name = "pLabel2";
-         lab.Location = new System.Drawing.Point(66, 40);
-         lab.Text = "Ez itt ?";
-         lab.AutoSize = true;
-         sc.Panel2.Controls.Add(lab);
-         sc.Panel2.Controls.SetChildIndex(lab, 1);
-
-
-     });
-
-            //Check settings.
-
-            if (Settings.Instance["TensionerURL"] == null)
+            //Since the controls on FlighData are located in a different thread, we must use BeginInvoke to access them.
+            MainV2.instance.BeginInvoke((MethodInvoker)(() =>
             {
-                Settings.Instance["TensionerURL"] = "http://192.168.0.1/data.xml";
-                urlTensionerAddress = "http://192.168.0.1/data.xml";
-            }
-            else
-            { 
-                urlTensionerAddress = Settings.Instance["TensionerURL"];
-            }
 
-            if (Settings.Instance["TensionerWebTimeout"] == null)
-            {
-                Settings.Instance["TensionerWebTimeout"] = "50";
-                webTimeoutMs=50;
-            }
-            else
-            {
-                webTimeoutMs = Settings.Instance.GetInt32("TensionerWebTimeout");
-            }
+                //SplitContainer1 is hosting panel1 and panel2 where panel2 contains the map and all other controls on the map (WindDir, gps labels, zoom, joystick, etc.)
+                FDRightSide = Host.MainForm.FlightData.Controls.Find("splitContainer1", true).FirstOrDefault() as SplitContainer;
 
+                //Create a new label to display debug info (this is not the label which display ground and air tension forces, it is used only for debug purposes)
+                // TODO: Make debug label switchable via menu/gui
+                lDebugInfo = new System.Windows.Forms.Label();
+                lDebugInfo.Name = "lDebugInfo";
+
+                //This is a good approximate position beside the wind direction and below the distance bar
+                lDebugInfo.Location = new System.Drawing.Point(66, 50);
+                lDebugInfo.Text = "TensionerDebugInfo";
+                lDebugInfo.AutoSize = true;
+
+                //Add to Panel2 and bring forward to display above the gMap
+                FDRightSide.Panel2.Controls.Add(lDebugInfo);
+                FDRightSide.Panel2.Controls.SetChildIndex(lDebugInfo, 1);
+            }));
+
+            //Check settings and save back (in case there are no initial values in config.xml
+
+            urlTensionerAddress = Settings.Instance["TensionerURL", "http://localhost/data.xml"];
+            Settings.Instance["TensionerURL"] = urlTensionerAddress;
+            webTimeoutMs = Settings.Instance.GetInt32("TensionerWebTimeout", 50);
+            Settings.Instance["TensionerWebTimeout"] = webTimeoutMs.ToString();
 
             return true;
         }
@@ -107,22 +90,25 @@ namespace RACPluginTensionerStat
 
         public override bool Loaded()
         {
-			Host.comPort.OnPacketReceived += MavOnOnPacketReceivedHandler;
+            //Catch all incoming Mavlink packets.
+            Host.comPort.OnPacketReceived += MavOnOnPacketReceivedHandler;
             return true;
         }
 
         public override bool Loop()
         {
 
+            //TODO: Fix up dronestatus bit 0 = cable released (1-yes, 0-no)
+            //                         bit 1 = Initial height reached (height is settable) (1-yes, 0-no)
 
+            //This is all temporary, need to clean it up
             string dronestatus = "0";
-            if (Host.cs.alt > 10) dronestatus = "1";
+            if (Host.cs.alt > 10) dronestatus = "2";
 
             string sTensionerInfo;
-            sTensionerInfo = "At Drone:" + tension_value.ToString("0.0") + "N ";
+            sTensionerInfo = String.Format("At Drone:{0}N", tension_value.ToString("0.0"));
+            string urlReq = String.Format("{0}?dronefoce={1}&dronestatus={2}", urlTensionerAddress, tension_value.ToString("0"), dronestatus);
 
-
-            string urlReq = urlTensionerAddress + "?droneforce=" + tension_value.ToString("0") + "&dronestatus=" + dronestatus;
             string retval = GetWinch(urlReq);
 
 
@@ -133,46 +119,41 @@ namespace RACPluginTensionerStat
                 var ropelength = xDoc.Descendants("ropelength").Single();
                 var tensionerstatus = xDoc.Descendants("tensionerstatus").Single();
 
-                //Console.WriteLine(tensionerforce.Value);
-                //Console.WriteLine(ropelength.Value);
-                //Console.WriteLine(tensionerstatus.Value);
-
-                sTensionerInfo = sTensionerInfo + "At winch:" + tensionerforce.ToString() + "N RopeOut:" + ropelength.ToString() + "m Stat:" + tensionerstatus.ToString();
-
+                sTensionerInfo = String.Format("{0} At Ground:{1}N RopeOut:{2}m GroundStat:{3}",
+                                                sTensionerInfo, tensionerforce.Value.ToString().Trim(),
+                                                ropelength.Value.ToString().Trim(),
+                                                tensionerstatus.Value.ToString().Trim());
             }
 
             //Must use BeginIvoke to avoid deadlock with OnClose in main form.
-            MainV2.instance.BeginInvoke((Action)(() =>
+            MainV2.instance.BeginInvoke((MethodInvoker)(() =>
             {
-                lab.Text = sTensionerInfo;
+                lDebugInfo.Text = sTensionerInfo;
             }));
-
-
-
             return true;
         }
- 
+
         public override bool Exit()
         {
-
-         return true;
+            return true;
         }
-		
-		
-		 private void MavOnOnPacketReceivedHandler(object o, MAVLink.MAVLinkMessage linkMessage)
+
+
+        private void MavOnOnPacketReceivedHandler(object o, MAVLink.MAVLinkMessage linkMessage)
         {
 
+            //Pull force at the drone is coming down as a Mavlink DEBUG message
+            //TODO: Add checking for  status.ind since we can send more than one type of DEBUG message in the future.
 
             if ((MAVLink.MAVLINK_MSG_ID)linkMessage.msgid == MAVLink.MAVLINK_MSG_ID.DEBUG)
             {
-
                 var status = linkMessage.ToStructure<MAVLink.mavlink_debug_t>();
                 tension_value = status.value;
-
-                //Host.cs.messages.Add((DateTime.Now, "Tension:"+tension_value.ToString()+" Newton"));
             }
         }
 
+
+        //TODO: Check if we can do it asyncronously (no need to que the data, but no need to block the plugin thread)
 
         public string GetWinch(string uri)
         {
@@ -181,7 +162,6 @@ namespace RACPluginTensionerStat
 
             try
             {
-
                 using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
                 using (Stream stream = response.GetResponseStream())
                 using (StreamReader reader = new StreamReader(stream))
@@ -192,14 +172,8 @@ namespace RACPluginTensionerStat
             catch
             {
                 return null;
-
             }
-
-
         }
-
-
-
     }
 }
 
