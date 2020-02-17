1d00f448d649f19d14d8e55a80b8febb3f448439
Andras Schaffer <eosbandi@gmail.com>
2020-02-17 21:49:28 +0100

    Add large display of tension force

--- RACPluginTensionerStat.cs
+++ RACPluginTensionerStat.cs
@@ -29,6 +29,7 @@ namespace RACPluginTensionerStat
 
         SplitContainer FDRightSide;
         Label lDebugInfo;
+        Label lPullForce;
         float tension_value;
         string urlTensionerAddress;
         int webTimeoutMs;
@@ -62,11 +63,20 @@ namespace RACPluginTensionerStat
                 //SplitContainer1 is hosting panel1 and panel2 where panel2 contains the map and all other controls on the map (WindDir, gps labels, zoom, joystick, etc.)
                 FDRightSide = Host.MainForm.FlightData.Controls.Find("splitContainer1", true).FirstOrDefault() as SplitContainer;
 
+                //hide direction color description labels
+                var l = Host.MainForm.FlightData.Controls.Find("label3", true).FirstOrDefault() as Label;
+                l.Visible = false;
+                l = Host.MainForm.FlightData.Controls.Find("label4", true).FirstOrDefault() as Label;
+                l.Visible = false;
+                l = Host.MainForm.FlightData.Controls.Find("label5", true).FirstOrDefault() as Label;
+                l.Visible = false;
+                l = Host.MainForm.FlightData.Controls.Find("label6", true).FirstOrDefault() as Label;
+                l.Visible = false;
+
                 //Create a new label to display debug info (this is not the label which display ground and air tension forces, it is used only for debug purposes)
                 // TODO: Make debug label switchable via menu/gui
                 lDebugInfo = new System.Windows.Forms.Label();
                 lDebugInfo.Name = "lDebugInfo";
-
                 //This is a good approximate position beside the wind direction and below the distance bar
                 lDebugInfo.Location = new System.Drawing.Point(66, 50);
                 lDebugInfo.Text = "TensionerDebugInfo";
@@ -75,6 +85,21 @@ namespace RACPluginTensionerStat
                 //Add to Panel2 and bring forward to display above the gMap
                 FDRightSide.Panel2.Controls.Add(lDebugInfo);
                 FDRightSide.Panel2.Controls.SetChildIndex(lDebugInfo, 1);
+
+                lPullForce = new Label();
+                lPullForce.Name = "lbl_PullForce";
+                lPullForce.Location = new System.Drawing.Point(0, FDRightSide.Panel2.Height-140);
+                lPullForce.Text = "000";
+                lPullForce.AutoSize = true;
+                lPullForce.Font = new Font("Tahoma", 45, FontStyle.Bold);
+                lPullForce.Anchor = (AnchorStyles.Bottom | AnchorStyles.Left);
+
+                FDRightSide.Panel2.Controls.Add(lPullForce);
+                FDRightSide.Panel2.Controls.SetChildIndex(lPullForce, 1);
+
+
+
+
             }));
 
             //Check settings and save back (in case there are no initial values in config.xml
@@ -124,11 +149,19 @@ namespace RACPluginTensionerStat
                                                 ropelength.Value.ToString().Trim(),
                                                 tensionerstatus.Value.ToString().Trim());
             }
-
             //Must use BeginIvoke to avoid deadlock with OnClose in main form.
             MainV2.instance.BeginInvoke((MethodInvoker)(() =>
             {
                 lDebugInfo.Text = sTensionerInfo;
+                lPullForce.Text = String.Format("{0:000}", tension_value);
+            //TODO: Coloring Green/Ywllow/RED
+
+                if (tension_value > 70) {
+                    lPullForce.ForeColor = Color.Red;
+                } else
+                {
+                    lPullForce.ForeColor = Color.Green;
+                }
             }));
             return true;
         }
